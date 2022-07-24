467b4e0005455ff02a1464762f7b28ece8826496
Renope <r.nouripoor@gmail.com>
2022-07-24 14:07:39 +0430

    restricted finished

--- contracts/BEP20/TransferControl.sol
+++ contracts/BEP20/TransferControl.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
 abstract contract TransferControl is ERC20, Ownable {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
 
-    struct Restriction {
-        uint256 monthlyAmount;
+    EnumerableMap.AddressToUintMap restrictedAddresses;
+
+    struct Checkpoint {
         uint256 spentAmount;
         uint256 nonce;
     }
 
-    mapping(address => Restriction) restrictedAddresses;
+    mapping(address => Checkpoint) checkpoints;
 
     uint256 monthlyTransferFraction;
     uint256 startTime;
@@ -21,26 +24,32 @@ abstract contract TransferControl is ERC20, Ownable {
         startTime = block.timestamp;
     }
 
-    function restrict(address addr, uint256 monthlyTransfers) public onlyOwner {
-        restrictedAddresses[addr].monthlyAmount = monthlyTransfers;
+    // restricted addresses
+    function restrict(address addr, uint256 amount) public onlyOwner {
+        restrictedAddresses.set(addr, amount);
     }
-
     function destrict(address addr) public onlyOwner {
-        delete restrictedAddresses[addr];
+        restrictedAddresses.remove(addr);
     }
 
-    function canSpend(address addr) public view returns(uint256 ) {
-        return restrictedAddresses[addr].monthlyAmount != 0 ?
-            restrictedAddresses[addr].monthlyAmount - restrictedAddresses[addr].spentAmount :
-            balanceOf(addr) * monthlyTransferFraction / 10 ** 6;
+
+    function canSpend(address addr) public view returns(uint256) {
+        return isRestricted(addr) ?
+            restrictedAddresses.get(addr) :
+            balanceOf(addr) * monthlyTransferFraction / 10 ** 6 - 
+                checkpoints[addr].spentAmount;
     }
 
     function isRestricted(address addr) public view returns(bool) {
-        return restrictedAddresses[addr].monthlyAmount != 0;
+        return restrictedAddresses.contains(addr);
     }
 
-    function _spend(address restrictedAddr, uint256 amount) internal {
-
+    function _spend(address addr, uint256 amount) internal {
+        if(isRestricted(addr)) {
+            uint256 spendableAmount = restrictedAddresses.get(addr);
+            require(amount <= spendableAmount, "amount exceeds spend limit");
+            restrictedAddresses.set(addr, spendableAmount - amount);
+        }
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
