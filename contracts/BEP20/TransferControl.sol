b68f562ae0b0fd643235030a768fccc3b4c24bf4
Renope <r.nouripoor@gmail.com>
2022-07-25 21:21:38 +0430

    comments added

--- contracts/BEP20/TransferControl.sol
+++ contracts/BEP20/TransferControl.sol
@@ -5,6 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
+/**
+ * @title control BLB transfers.
+ * @notice users may have access to transfer their whole BLB balance or only a 
+ * certain fraction every period(it depends on periodFraction).
+ * @notice some specific addresses may have restricted access to transfer.
+ * @notice owner of the contract can restrict every desired address and also 
+ * determine the a spend limit for all users.
+ */
 abstract contract TransferControl is ERC20, Ownable {
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
@@ -26,23 +34,69 @@ abstract contract TransferControl is ERC20, Ownable {
         periodTime = 30 days;
     }
 
+    /**
+     * @notice set spend limit for period transfers.
+     * @notice there is no transfer limit if fraction is 10**6.
+     *
+     * @param fraction the numerator of transfer limit rate which denominator
+     * is 10**6.
+     *
+     * @notice require:
+     *  - only owner of contract can call this function.
+     *  - maximum fraction can be 10**6 (equal to 100%).
+     */
     function setperiodTransferFraction(uint256 fraction) public onlyOwner {
         require(fraction <= 10 ** 6, "maximum fraction is 10**6 (equal to 100%)");
         periodFraction = fraction;
     }
 
-    // restricted addresses
+    /**
+     * @notice restrict an address 
+     * @notice the address `addr` will be only able to spend as much as `amount`.
+     *
+     * @param addr the restricted address.
+     * @param amount restricted spendable amount.
+     *
+     * @notice require:
+     *  - only owner of contract can call this function.
+     */
     function restrict(address addr, uint256 amount) public onlyOwner {
         restrictedAddresses.set(addr, amount);
     }
+
+    /**
+     * @notice destrict an address 
+     * @notice the address `addr` will be free to spend their BLB like regular
+     * addresses.
+     *
+     * @param addr the address that is going to be destricted.
+     *
+     * @notice require:
+     *  - only owner of contract can call this function.
+     */
     function destrict(address addr) public onlyOwner {
         restrictedAddresses.remove(addr);
     }
 
+    /**
+     * @return boolean true if the address is restricted.
+     *
+     * @param addr the address that is going to be checked.
+     */
     function isRestricted(address addr) public view returns(bool) {
         return restrictedAddresses.contains(addr);
     }
 
+    /**
+     * @return amount that the address can spend.
+     * 
+     * @dev if the address restricted, the amount equals remaining spendable amount for the 
+     * address. else if there is a spend limit active for the contract, the amount equals 
+     * the address's remaining period spendable amount. else the amount equals balance of the
+     * address.
+     * 
+     * @param addr the address that is being checked.
+     */
     function canSpend(address addr) public view returns(uint256 amount) {
         if (isRestricted(addr)){
             return restrictedAddresses.get(addr);
@@ -80,7 +134,9 @@ abstract contract TransferControl is ERC20, Ownable {
         internal
         override
     {
-        _spend(from, amount);
+        if(_msgSender() != owner()) {
+            _spend(from, amount);
+        }
         super._beforeTokenTransfer(from, to, amount);
     }
 }
