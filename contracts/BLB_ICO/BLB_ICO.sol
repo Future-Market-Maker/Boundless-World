6f90181d68872e344af95be8319c817d34d376be
Renope <r.nouripoor@gmail.com>
2022-08-16 02:59:29 +0430

    events added

--- contracts/BLB_ICO/BLB_ICO.sol
+++ contracts/BLB_ICO/BLB_ICO.sol
@@ -16,11 +16,17 @@ contract BLB_ICO is Ownable {
     //aggregator on rinkeby (multiplied by 10^18)
     AggregatorInterface constant AGGREGATOR_DAI_ETH = AggregatorInterface(0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D);
 
+    event BuyInBNB(uint256 indexed amountBLB, uint256 indexed amountBNB);
+    event BuyInUSDT(uint256 indexed amountBLB, uint256 indexed amountUSDT);
+    event BuyInBUSD(uint256 indexed amountBLB, uint256 indexed amountBUSD);
+    event SetPriceInUSD(uint256 indexed newPrice);
+    event Withdraw(address indexed tokenAddr, uint256 indexed amount);
+
     constructor() {
         BLB = IERC20(0x314FbBFC5c9Db19BC8F8981781D326A9bA76508f); //BLB test on rinkeby
         BUSD = IERC20(0x5a47B08A3e5058CF3b68b583851CCf585718AE44);//simple ERC20 on rinkeby
         USDT = IERC20(0x76a90A822b4c797C0BfaED9453445241e5553D00);//simple ERC20 on rinkeby
-        set_priceInUSD(10 ** 18); // equals to 1 USD
+        setPriceInUSD(10 ** 18); // equals to 1 USD
     }
 
     /**
@@ -52,6 +58,7 @@ contract BLB_ICO is Ownable {
         require(msg.value >= amount * priceInBNB() * 98 / 10**20, "insufficient fee");
         require(BLB.balanceOf(address(this)) >= amount, "insufficient BLB in the contract");
         BLB.transfer(msg.sender, amount);
+        emit BuyInBNB(amount, msg.value);
     }
 
     /**
@@ -65,8 +72,10 @@ contract BLB_ICO is Ownable {
      */
     function buyInBUSD(uint256 amount) public {
         require(BLB.balanceOf(address(this)) >= amount, "insufficient BLB in the contract");
-        BUSD.transferFrom(msg.sender, address(this), priceInUSD * amount / 10**18); 
+        uint256 payableBUSD = priceInUSD * amount / 10 ** 18;
+        BUSD.transferFrom(msg.sender, address(this), payableBUSD); 
         BLB.transfer(msg.sender, amount);       
+        emit BuyInBUSD(amount, payableBUSD);
     }
 
     /**
@@ -80,8 +89,10 @@ contract BLB_ICO is Ownable {
      */
     function buyInUSDT(uint256 amount) public {
         require(BLB.balanceOf(address(this)) >= amount, "insufficient BLB in the contract");
-        USDT.transferFrom(msg.sender, address(this), priceInUSD * amount / 10**18);        
+        uint256 payableUSDT = priceInUSD * amount / 10 ** 18;
+        USDT.transferFrom(msg.sender, address(this), payableUSDT);        
         BLB.transfer(msg.sender, amount);       
+        emit BuyInUSDT(amount, payableUSDT);
     }
 
 
@@ -93,8 +104,9 @@ contract BLB_ICO is Ownable {
     * @notice requirement:
     *   - only owner of the contract can call this function.
     */
-    function set_priceInUSD(uint256 _priceInUSD) public onlyOwner {
+    function setPriceInUSD(uint256 _priceInUSD) public onlyOwner {
         priceInUSD = _priceInUSD;
+        emit SetPriceInUSD(_priceInUSD);
     }
 
     /**
@@ -105,6 +117,7 @@ contract BLB_ICO is Ownable {
     */
     function withdraw(address tokenAddr, uint256 amount) public onlyOwner {
         IERC20(tokenAddr).transfer(msg.sender, amount);
+        emit Withdraw(tokenAddr, amount);
     }
 
 
@@ -116,5 +129,6 @@ contract BLB_ICO is Ownable {
     */
     function withdraw(uint256 amount) public onlyOwner {
         payable(msg.sender).transfer(amount);
+        emit Withdraw(address(0), amount);
     }
 }
