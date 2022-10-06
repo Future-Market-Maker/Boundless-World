f9af9ea99c8219e0ee28b187a4a36927c99ce38e
Renope <r.nouripoor@gmail.com>
2022-10-06 16:27:45 +0330

    tested contract deployed to BSC testnet

--- scripts/deployBLBToken.js
+++ scripts/deployBLBToken.js
@@ -3,8 +3,9 @@ const { verify } = require("./utils/verifier.js")
 
   async function deployBLBToken() {
     
-    const initialAdmin = "0x31FBc230BC6b8cE2eE229eCfbACCc364Da3eD7fC";
-
+    // const initialAdmin = "0x31FBc230BC6b8cE2eE229eCfbACCc364Da3eD7fC";
+    const initialAdmin = "0xe189BfcC0D6f5f63401B104c1051699C7AA1ae4a";
+    
     const BLBToken = await ethers.getContractFactory("BLBToken");
     const BLB = await BLBToken.deploy(initialAdmin);
     await BLB.deployed();
