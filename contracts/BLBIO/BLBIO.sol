11f4c5c3862968023f069470c9601e2a8c1c8cfb
Renope <r.nouripoor@gmail.com>
2022-11-11 20:53:20 +0330

    comments added

--- contracts/BLBIO/BLBIO.sol
+++ contracts/BLBIO/BLBIO.sol
@@ -4,12 +4,25 @@ pragma solidity ^0.8.4;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorInterface.sol";
 import "./BLBIOAdministration.sol";
 
-
+/**
+ * @title BLB Initial Offering
+ *
+ * @dev BLB Token is offered in BNB and BUSD(USDT).
+ * @dev the prices are set in USD and calculated to corresponding BNB in 
+ *   every purchase transaction via chainlink price feed aggregator.
+ * @dev the bought blbs are locked in the contract until the Initial offering
+ *   ends. then the owner can unlock proper fraction to be claimed. 
+ * @dev there two sale plan; public sale price for small amounts and private sale
+ *  price for large amounts of blb.
+ * @dev since solidity does not support floating variables, all prices are
+ *   multiplied by 10^18 to embrace decimals.
+ */
 contract BLBIO is BLBIOAdministration {
 
+    //price feed aggregator
     AggregatorInterface immutable AGGREGATOR_BUSD_BNB;
 
-    bool public soldOut;
+    bool public soldOut; //false means users can purchase, true means blb sold out
     
     struct UserClaim {
         uint256 total;
@@ -37,41 +50,65 @@ contract BLBIO is BLBIOAdministration {
         ); 
     }
 
-    event BuyInBNB(
-        address indexed buyer,
-        uint256 amountBLB, 
-        uint256 amountBNB
+    /**
+     * @dev emits when a user purchases BLB.
+     */
+    event Purchase(
+        address indexed purchaser,
+        string indexed tokenPaid,
+        uint256 amountPaid,
+        uint256 amountBLB 
     );
-    event BuyInBUSD(
-        address indexed buyer,
-        uint256 amountBLB, 
-        uint256 amountBUSD
-    );
+
+    /**
+     * @dev emits when a user claims their unlocked BLB.
+     */
     event Claim(
         address indexed claimant,
         uint256 amountBLB
     );
 
+    /**
+     * @dev emits when SoldOut situation switches.
+     */
+    event SoldOut(bool situation);
+
 
 // get -------------------------------------------------------------------------
 
+    /**
+     * @return price of the token in USD.
+     *
+     * @notice the private and public price are calculated automatically.
+     */
     function priceInUSD(uint256 amount) public view returns(uint256) {
         require(!soldOut, "BLBIO: sold out!");
         return amount > retailLimit ? privatePriceInUSD : publicPriceInUSD
             * amount / 10 ** 18;
     }
 
+    /**
+     * @return price of the token in BNB corresponding to the USD price.
+     *
+     * @notice the private and public price are calculated automatically.
+     */
     function priceInBNB(uint256 amount) public view returns(uint256) {
         require(!soldOut, "BLBIO: sold out!");
         return uint256(AGGREGATOR_BUSD_BNB.latestAnswer())
             * priceInUSD(amount) / 10 ** 18;
     }
 
+    /**
+     * @return amount of the BLB token the user can claim.
+     */
     function totalClaimable(address claimant) public view returns(uint256) {
         UserClaim storage uc = userClaims[claimant];
         return uc.total - uc.claimed;
     }
 
+    /**
+     * @return amount of the BLB token the user can claim just now.
+     */
     function claimable(address claimant) public view returns(uint256) {
         UserClaim storage uc = userClaims[claimant];
         
@@ -85,24 +122,50 @@ contract BLBIO is BLBIOAdministration {
 
 // set -------------------------------------------------------------------------
 
-    function buyInBNB(uint256 amount) public payable {
-        address buyer = msg.sender;
+    /**
+     * @dev purchase BLB Token paying in BNB.
+     *
+     * @notice maximum tolerance 2%.
+     *
+     * @notice requirement:
+     *   - required amount must be paid in BNB.
+     *
+     * @notice emits a Purchase event
+     */
+    function purchaseInBNB(uint256 amount) public payable {
+        address purchaser = msg.sender;
         require(msg.value >= priceInBNB(amount) * 98/100, "insufficient fee");
-        userClaims[buyer].total += amount;
+        userClaims[purchaser].total += amount;
         TotalClaimable += amount;
-        emit BuyInBNB(buyer, amount, msg.value);
+        emit Purchase(purchaser, "BNB", msg.value, amount);
     }
 
-    function buyInBUSD(uint256 amount) public {
-        address buyer = msg.sender;
-        require(BLB.balanceOf(address(this)) >= amount, "insufficient BLB in the contract");
+    /**
+     * @dev purchase BLB Token paying in BUSD.
+     *
+     * @notice requirement:
+     *   - Purchaser must approve the ICO to spend required BUSD.
+     *
+     * @notice emits a Purchase event
+     */
+    function purchaseInBUSD(uint256 amount) public {
+        address purchaser = msg.sender;
         uint256 payableBUSD = priceInUSD(amount);
-        BUSD.transferFrom(buyer, address(this), payableBUSD); 
-        userClaims[buyer].total += amount;       
+        BUSD.transferFrom(purchaser, address(this), payableBUSD); 
+        userClaims[purchaser].total += amount;       
         TotalClaimable += amount;
-        emit BuyInBUSD(buyer, amount, payableBUSD);
+        emit Purchase(purchaser, "BUSD", payableBUSD, amount);
     }
 
+    /**
+     * @dev transfer unlocked BLBs to the claimant.
+     *
+     * @notice requirement:
+     *   - claimable amount should not be zero.
+     *   - there must be sufficient BLB token in this contract.
+     *
+     * @notice emits a Claim event
+     */
     function claim() public {
         address claimant = msg.sender; 
         UserClaim storage uc = userClaims[claimant];
@@ -118,11 +181,31 @@ contract BLBIO is BLBIOAdministration {
         emit Claim(claimant, _claimable);      
     }
 
-    
+    /**
+     * @dev turns the contract's offering on or off.
+     *
+     * @notice requirement:
+     *   - only owner of the contract can call this function.
+     * 
+     * @notice emits a SoldOut event
+     */
     function setSoldOut() public onlyOwner {
         soldOut = soldOut ? false : true;
+
+        emit SoldOut(soldOut);
     }
     
+    /**
+     * @dev gift some BLBs to desired user.
+     *
+     * @notice requirement:
+     *   - only owner of the contract can call this function.
+     * 
+     * @notice the user who earned BLB may be able to claim or may has to wait just 
+     *   like other users. 
+     * 
+     * @notice emits a Purchase event
+     */
     function giftBLB(
         address addr, 
         uint256 amount, 
@@ -131,5 +214,6 @@ contract BLBIO is BLBIOAdministration {
         userClaims[addr].total += amount; 
         userClaims[addr].freeToClaim = freeToClaim; 
         TotalClaimable += amount;
+        emit Purchase(addr, "gift", 0, amount);
     }
 }
